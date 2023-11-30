946ba53f8270f261083e6874fa3aa9ec88aede3e
htbt2003 <phuong7934836@gmail.com>
2023-11-30 15:16:30 +0700

    tuan02

--- huynhthibaotran_frontend_cdtt/src/config.js
+++
@@ -1,2 +0,0 @@
-export const urlAPI= "http://localhost/public/api/"
-export const urlImage= "http://localhost/public/images/"
--- huynhthibaotran_frontend_cdtt/src/httpAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { urlAPI } from "./config";
-
-const httpAxios = axios.create({
-    baseURL: urlAPI,
-    timeout: 20000,
-    header: {"X-Customer_Header":'foobar'}
-})
-export default httpAxios;
